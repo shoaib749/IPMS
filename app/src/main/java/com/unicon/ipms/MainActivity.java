96f79c1c776332eb5864ffc97987ffbe3aba57b0
Tawfeek <khantawfeek00@gmail.com>
2022-02-15 06:27:02 -0800

    Create an account some xml file added

--- app/src/main/java/com/unicon/ipms/MainActivity.java
+++ app/src/main/java/com/unicon/ipms/MainActivity.java
@@ -2,13 +2,31 @@ package com.unicon.ipms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button Student;
+    private Button Admin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Student=findViewById(R.id.Student);
+        Admin=findViewById(R.id.Admin);
+        Student.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+             openActivity2();
+            }
+
+        });
+    }
+    public void openActivity2(){
+        Intent intent =new Intent(this,Create_Info.class);
+        startActivity(intent);
     }
 }
