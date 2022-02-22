63340bb1ab8d91408a7d7dbfc00b14244d968bd7
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-02-23 00:11:07 +0530

    Admin login connected to db

--- app/src/main/java/com/unicon/ipms/constant.java
+++ app/src/main/java/com/unicon/ipms/constant.java
@@ -1,8 +1,9 @@
 package com.unicon.ipms;
 
 public class constant {
-    public static final String ROOT_URL="http://192.168.137.1/IPMS_Script/v1/"; //TODO change this
+    public static final String ROOT_URL="http://192.168.29.50/IPMS_Script/v1/"; //TODO change this
     public static final String URL_REGISTER =ROOT_URL+"userRegistration.php";
     public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
     public static final String URL_MOREINFO = ROOT_URL+"moreInfo.php";
+    public static final String URL_ADMIN_LOGIN = ROOT_URL+"adminLogin.php";
 }
