8fd03f8465476ecf4b6986811d9c421640cf7378
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-02-23 11:33:15 +0530

    added admin dashboard

---
+++ app/src/main/java/com/unicon/ipms/admin_dashboard.java
@@ -0,0 +1,14 @@
+package com.unicon.ipms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class admin_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_dashboard);
+    }
+}
