034c0403c36f256ec4798815f876f0f0fced46f1
harsh <harshitsathone@gmail.com>
2022-02-24 08:00:54 +0530

    made admin_dashboard template, added icons

--- app/src/main/java/com/unicon/ipms/admin_login.java
+++ app/src/main/java/com/unicon/ipms/admin_login.java
@@ -1,15 +1,95 @@
 package com.unicon.ipms;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class admin_login extends AppCompatActivity {
-
+    private EditText ET_email_id,ET_pass;
+    private Button B_login;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
         getSupportActionBar().hide();
+
+        ET_email_id = (EditText) findViewById(R.id.login_username);
+        ET_pass = (EditText) findViewById(R.id.login_password);
+        B_login = (Button) findViewById(R.id.sign_in);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait....");
+        B_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getData();
+            }
+        });
+    }
+    public void getData(){
+
+        String email_id,pass;
+        email_id = ET_email_id.getText().toString().trim();
+        pass = ET_pass.getText().toString().trim();
+        progressDialog.show();
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                constant.URL_ADMIN_LOGIN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                startActivity(new Intent(admin_login.this,admin_dashboard.class));
+                progressDialog.dismiss();
+                try {
+                    JSONObject object = new JSONObject(response);
+                    if(!object.getBoolean("error")){
+                        Toast.makeText(admin_login.this, "User Login successful", Toast.LENGTH_SHORT).show();
+
+
+                    }else{
+                        Toast.makeText(admin_login.this,object.getString("message"),Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(admin_login.this,"error1"+e,Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.hide();
+                Toast.makeText(admin_login.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("email_id",email_id);
+                params.put("pass",pass);
+                return params;
+            }
+        };
+        requestHandler.getInstance(this).addToRequestQueue(stringRequest);
     }
 }
--- app/src/main/java/com/unicon/ipms/constant.java
+++ app/src/main/java/com/unicon/ipms/constant.java
@@ -1,8 +1,9 @@
 package com.unicon.ipms;
 
 public class constant {
-    public static final String ROOT_URL="http://192.168.137.1/IPMS_Script/v1/"; //TODO change this
+    public static final String ROOT_URL="http://192.168.0.106/IPMS_Script/v1/"; //TODO change this, no spaces
     public static final String URL_REGISTER =ROOT_URL+"userRegistration.php";
     public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
     public static final String URL_MOREINFO = ROOT_URL+"moreInfo.php";
+    public static final String URL_ADMIN_LOGIN = ROOT_URL+"adminLogin.php";
 }
