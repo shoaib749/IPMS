cfe0c5f20f62c5c9489344c8f0e67b9f0562231f
Tawfeek <khantawfeek00@gmail.com>
2022-02-26 20:00:56 -0800

    Student_dashboard added

---
+++ app/src/main/java/com/unicon/ipms/Student_DashBoard_UI/Placement_status/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.unicon.ipms.Student_DashBoard_UI.Placement_status;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
