e42c4001e52a39cac8fb3bf6431ccdf1104ab4b1
Tawfeek <khantawfeek00@gmail.com>
2022-02-26 21:14:41 -0800

    updated

--- app/src/main/java/com/unicon/ipms/student_dashboard.java
+++ app/src/main/java/com/unicon/ipms/student_dashboard.java
@@ -41,7 +41,7 @@ public class student_dashboard extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_placements, R.id.nav_internships, R.id.nav_placement_status)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_student_dashboard);
