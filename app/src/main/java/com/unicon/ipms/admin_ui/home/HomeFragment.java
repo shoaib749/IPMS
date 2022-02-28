f0b71f85de963d74783baa41c0ab4232156088bc
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-02-28 22:09:25 +0530

    Added CountOffers

--- app/src/main/java/com/unicon/ipms/admin_ui/home/HomeFragment.java
+++ app/src/main/java/com/unicon/ipms/admin_ui/home/HomeFragment.java
@@ -1,16 +1,32 @@
 package com.unicon.ipms.admin_ui.home;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.unicon.ipms.constant;
 import com.unicon.ipms.databinding.AdminFragmentHomeBinding;
+import com.unicon.ipms.requestHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
@@ -24,8 +40,43 @@ public class HomeFragment extends Fragment {
         binding = AdminFragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.TVTotalOfferCount;
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                constant.URL_ADMIN_COUNT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            if(!obj.getBoolean("error")){
+                                textView.setText(obj.getString("TotalOfferCount"));
+                            }else{
+                                Toast.makeText(getContext(), obj.getString("message"), Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getContext(), "error"+e, Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+        ){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                return  params;
+            }
+        };
+        requestHandler.getInstance(getContext()).addToRequestQueue(stringRequest);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
