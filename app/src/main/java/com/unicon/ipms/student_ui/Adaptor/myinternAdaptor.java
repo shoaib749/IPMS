078de2f2b05fba9ef735946aba8c023cb97158a2
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-04-17 15:59:15 +0530

    Student Internship stat end date connected to db

--- app/src/main/java/com/unicon/ipms/student_ui/Adaptor/myinternAdaptor.java
+++ app/src/main/java/com/unicon/ipms/student_ui/Adaptor/myinternAdaptor.java
@@ -15,9 +15,6 @@ import com.unicon.ipms.myIntenshipDateModel;
 import java.util.List;
 
 public class myinternAdaptor extends RecyclerView.Adapter<myinternAdaptor.holder> {
-
-    String data[];
-    String sd[], ed[];
     List<myIntenshipDateModel> model;
     LayoutInflater inflater;
 
@@ -29,20 +26,12 @@ public class myinternAdaptor extends RecyclerView.Adapter<myinternAdaptor.holder
     @NonNull
     @Override
     public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-//        LayoutInflater inflator = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.student_my_internship_cardview, parent, false);
         return new holder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull holder holder, int position) {
-//        String title = data[position];
-//        String sd_1 = sd[position];
-//        String ed_1 = ed[position];
-//        holder.tv.setText(title);
-//        holder.sd.setText(sd_1);
-//        holder.ed.setText(ed_1);
         holder.tv.setText(model.get(position).getCompany_name());
         holder.sd.setText(model.get(position).getStart_date());
         holder.ed.setText(model.get(position).getEnd_date());
--- app/src/main/java/com/unicon/ipms/student_ui/student_my_Internships_fragment.java
+++ app/src/main/java/com/unicon/ipms/student_ui/student_my_Internships_fragment.java
@@ -1,24 +1,49 @@
 package com.unicon.ipms.student_ui;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.unicon.ipms.R;
+import com.unicon.ipms.constant;
 import com.unicon.ipms.databinding.FragmentStudentMyInternshipsFragmentBinding;
 import com.unicon.ipms.databinding.StudentFragmentInternshipsBinding;
+import com.unicon.ipms.getStudentPlacementSalModel;
+import com.unicon.ipms.myIntenshipDateModel;
+import com.unicon.ipms.sharedPrefManager;
 import com.unicon.ipms.student_ui.Adaptor.myinternAdaptor;
 import com.unicon.ipms.student_ui.Adaptor.recyclerviewadaptor;
+import com.unicon.ipms.student_ui.Adaptor.yourplaceAdaptor;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class student_my_Internships_fragment extends Fragment {
-
+    List<myIntenshipDateModel> model_list = new ArrayList<myIntenshipDateModel>();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +54,56 @@ public class student_my_Internships_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FragmentStudentMyInternshipsFragmentBinding binding = FragmentStudentMyInternshipsFragmentBinding.inflate(inflater, container, false);
-        RecyclerView company_list=binding.myInternshipLV;
-        String[] list={"Capgemini","TCS","Infosys","Infocept","Persistent","Amazon","Google","Facebook"};
-        String[] start={"10-09-2021","12-05-2021","-","-","-","-","-","-"};
-        String[] end={"10-12-2021","12-08-2021","-","-","-","-","-","-"};
-        company_list.setLayoutManager(new LinearLayoutManager(this.getContext()));
-//        company_list.setAdapter(new myinternAdaptor(list,start,end));
+        RecyclerView recyclerView=binding.myInternshipLV;
+        ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                constant.URL_STUDENT_INTENSHIP_DATE,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        JSONArray response1;
+                        try {
+                            response1= new JSONArray(response);
+                            for(int i=0;i<response1.length();i++){
+                                JSONObject object = response1.getJSONObject(i);
+                                myIntenshipDateModel model = new myIntenshipDateModel();
+                                model.setCompany_name(object.getString("company_name"));
+                                model.setStart_date(object.getString("start_date"));
+                                model.setEnd_date(object.getString("end_date"));
+                                model_list.add(model);
+
+                            }
+
+                        } catch (JSONException e) {
+                            progressDialog.dismiss();
+                            e.printStackTrace();
+                        }
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                        recyclerView.setAdapter(new myinternAdaptor(getContext(),model_list));
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                Toast.makeText(getContext(),"error"+error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }
+        ) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email_id", sharedPrefManager.getInstance(getContext()).getEmail());
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
         return binding.getRoot();
     }
 }
